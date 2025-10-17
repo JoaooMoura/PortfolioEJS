10be851b060229ab7633c86ad6bcc6eff8122726
João Moura <jv.moura.sjc@gmail.com>
2025-10-17 00:12:20 -0300

    fix: Rotas de editar/excluir

--- src/controllers/portfolioController.js
+++ src/controllers/portfolioController.js
@@ -5,6 +5,7 @@ const projects = require('../data/projects');
 const estudante = require('../data/estudante');
 const disciplinas = require('../data/disciplinas');
 const contato = require('../data/contato');
+const { promiseHooks } = require('v8');
 
 const getHomePage = (req, res) => {
     res.render('pages/index', {
@@ -52,6 +53,66 @@ const addNovoProjeto = (req, res) => {
         res.redirect('/projetos');
     });
 };
+const excluirProjeto = (req, res) => {
+    const idParaExcluir = Number(req.params.id);
+
+    const indexParaExcluir = projects.findIndex(p => p.id === idParaExcluir);
+
+    if (indexParaExcluir > -1) {
+        projects.splice(indexParaExcluir, 1); 
+    }
+
+    const filePath = path.join(__dirname, '..', 'data', 'projects.js');
+
+    const updatedData = `module.exports = ${JSON.stringify(projects, null, 4)};`;
+
+    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+        if (err) {
+            console.error("Erro ao salvar o arquivo após exclusão:", err);
+        } else {
+            console.log("Projeto excluído e arquivo salvo com sucesso!");
+        }
+        res.redirect('/projetos');
+    });
+};
+const getFormularioEditarProjeto = (req, res) => {
+    const idParaEditar = Number(req.params.id);
+
+    const projeto = projects.find(p => p.id === idParaEditar);
+    if (!projeto) {
+        return res.redirect('/projetos');
+    }
+
+    res.render('pages/editarProjeto', {
+        title: 'Editar Projeto',
+        projeto: projeto
+    });
+};
+const salvarProjetoEditado = (req, res) => {
+    const idParaEditar = Number(req.params.id);
+    const dadosAtualizados = req.body;
+
+    const indexParaEditar = projects.findIndex(p => p.id === idParaEditar);
+
+    if (indexParaEditar > -1) {
+        projects[indexParaEditar].title = dadosAtualizados.title;
+        projects[indexParaEditar].description = dadosAtualizados.description;
+        projects[indexParaEditar].imageUrl = dadosAtualizados.imageUrl;
+        projects[indexParaEditar].link = dadosAtualizados.link;
+    }
+
+    const filePath = path.join(__dirname, '..', 'data', 'projects.js');
+    const updatedData = `module.exports = ${JSON.stringify(projects, null, 4)};`;
+
+    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+        if (err) {
+            console.error("Erro ao salvar o projeto editado:", err);
+        } else {
+            console.log("Projeto editado e salvo com sucesso!");
+        }
+        res.redirect('/projetos');
+    });
+};
 const getFormularioNovaDisc = (req, res) => {
     res.render('pages/cadastroDisciplinas', {
         title: 'Adicionar Disciplina'
@@ -75,6 +136,63 @@ const addNovaDisc = (req, res) => {
         res.redirect('/disciplinas');
     });
 };
+const excluirDisciplina = (req, res) => {
+    const indexParaExcluir = Number(req.params.index);
+
+    disciplinas.splice(indexParaExcluir, 1);
+
+    const filePath = path.join(__dirname, '..', 'data', 'disciplinas.js');
+
+    const updatedData = `module.exports = ${JSON.stringify(disciplinas, null, 4)};`;
+
+    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+        if (err) {
+            console.error("Erro ao excluir disciplina:", err);
+        } else {
+            console.log("Disciplina excluída com sucesso!");
+        }
+        res.redirect('/disciplinas');
+    });
+};
+
+
+
+const getFormularioEditarDisciplina = (req, res) => {
+    
+    const indexParaEditar = Number(req.params.index);
+
+    
+    const disciplina = disciplinas[indexParaEditar];
+
+    
+    res.render('pages/editarDisciplinas', {
+        title: 'Editar Disciplina',
+        disciplina: disciplina,
+        index: indexParaEditar 
+    });
+};
+const salvarDisciplinaEditada = (req, res) => {
+    
+    const indexParaEditar = Number(req.params.index);
+    const novoNome = req.body.nome;
+
+    
+    disciplinas[indexParaEditar] = novoNome;
+
+    
+    const filePath = path.join(__dirname, '..', 'data', 'disciplinas.js');
+    const updatedData = `module.exports = ${JSON.stringify(disciplinas, null, 4)};`;
+
+    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+        if (err) {
+            console.error("Erro ao salvar a disciplina editada:", err);
+        } else {
+            console.log("Disciplina editada e salva com sucesso!");
+        }
+        res.redirect('/disciplinas');
+    });
+};
+
 const getDisciplinasPage = (req, res) => {
     res.render('pages/disciplinas', {
         title: 'Minhas Disciplinas',
@@ -115,5 +233,11 @@ module.exports = {
     getFormularioNovoProjeto,
     addNovoProjeto,
     getFormularioNovaDisc,
-    addNovaDisc
+    addNovaDisc,
+    excluirProjeto,
+    excluirDisciplina,
+    getFormularioEditarProjeto,
+    salvarProjetoEditado,
+    getFormularioEditarDisciplina,
+    salvarDisciplinaEditada
 };
